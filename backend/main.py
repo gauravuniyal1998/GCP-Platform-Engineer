4b15e2e49eb75d6cba156cc0e2037985817d69be
gauravuniyal1998 <gauravuniyal1998@gmail.com>
2025-08-05 13:25:27 +0530

    added the fronend and backend contect files. updated the basic HTML landing page and basic docker file configurations

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,15 @@
 # main.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/status')
+def status():
+    return jsonify({'status': 'Backend is running successfully!'})
+
+@app.route('/')
+def home():
+    return "Hello from Flask backend on GCP!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
