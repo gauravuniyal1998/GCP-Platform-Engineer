5aa34fa6a2615ad03463ebb4bd0d4aca09bef2be
Gaurav Uniyal <gauravuniyal1998@gmail.com>
2025-08-17 17:48:40 +0530

    Delete: deleting the entire terraform dir

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-# main.tf
-resource "google_storage_bucket" "terraform_bucket" {   
-  name          = "${var.project_id}-terraform-bucket"
-  location      = var.region
-  force_destroy = true
-  uniform_bucket_level_access = true
-}
-
--- terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-project_id = "indigo-skyline-467604-d6"
-region = "asia-south2"
-zone = "asia-south2-a"
