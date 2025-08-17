c2ce6c46236ca302f0d276686e6d9065ce551890
gauravuniyal1998 <gauravuniyal1998@gmail.com>
2025-08-17 17:37:36 +0530

    update: updated the .gitignore files to stop staging terraform files.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,8 @@
 # main.tf
+resource "google_storage_bucket" "terraform_bucket" {   
+  name          = "${var.project_id}-terraform-bucket"
+  location      = var.region
+  force_destroy = true
+  uniform_bucket_level_access = true
+}
+
